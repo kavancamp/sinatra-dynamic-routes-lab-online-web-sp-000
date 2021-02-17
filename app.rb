7592e5c788d24067f8b1abfc1f029a4df0a95a7c
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-17 20:19:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
 
-end
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+  end
+  end
